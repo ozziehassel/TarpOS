17f47a13b9b4d06265abab608d27f62587478fcf
Ozzie Hassel <43375661+ozziehassel@users.noreply.github.com>
2021-01-02 17:06:19 -0500

    Created base files

---
+++ main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+  })
+  mainWindow.loadFile('window/index.html');
+  mainWindow.maximize();
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
