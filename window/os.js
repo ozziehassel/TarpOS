9e653bc12316585036c9139a653ff2ec3b640f9b
Ozzie Hassel <43375661+ozziehassel@users.noreply.github.com>
2021-01-03 09:21:51 -0500

    Added window titles and stylized close buttons

--- window/os.js
+++ window/os.js
@@ -5,29 +5,28 @@ var sys = {
  taskbar: ['Files', 'Photos', 'Settings'],
  desktop: [],
  settings: {
-     defaultWindowHeight: 30,
-     defaultWindowWidth: 30
+     defaultWindowHeight: 60,
+     defaultWindowWidth: 50
  }
 }
 function boot(){
     for(i=0;i<sys.taskbar.length;i++){
         var pinIcon = document.createElement('img');
         pinIcon.style = 'height: 5vh; width: 5vh; margin: 1vh;';
-        pinIcon.addEventListener('click', () => {
-            openPrgm(sys.taskbar[i]);
-        });
+        pinIcon.setAttribute('onclick', 'openPrgm("'+sys.taskbar[i]+'")');
+        console.log('openPrgm("'+sys.taskbar[i]+'")');
         document.getElementById('taskbar').appendChild(pinIcon);
     }
 }
 function openPrgm(name){
     var window = document.createElement('div');
+    window.className = 'win';
     window.style.height = sys.settings.defaultWindowHeight + 'vh';
     window.style.width = sys.settings.defaultWindowWidth + 'vw';
-    window.style.backgroundColor = 'white';
-    window.style.padding = '0';
-    window.style.position = 'absolute';
     var close = document.createElement('button');
-    close.style = 'outline: 0; border: 1px solid black; position: absolute; top: 0; right: 0; width: 3vw; height: 2.5vh; background: 0;';
+    window.innerHTML = '<span>'+name+'</span>'
+    close.className = 'closeBtn';
+    close.innerHTML = 'X';
     window.appendChild(close);
     close.addEventListener('click', () => {
         closePrgm(name, window);
@@ -41,5 +40,6 @@ function openPrgm(name){
 function closePrgm(name, window){
     window.style.display = 'none';
     sys.processes.splice(sys.processes.indexOf(name), 1);
+    console.log(name);
 }
 boot();
