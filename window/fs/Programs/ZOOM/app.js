575e8761104eb4a37435bafca4ba3b20113c88ab
Yikuan Sun <yikuansun2015@gmail.com>
2021-01-05 17:04:59 -0500

    fixed zoom

--- window/fs/Programs/ZOOM/app.js
+++ window/fs/Programs/ZOOM/app.js
@@ -2,4 +2,5 @@ function joinMeeting(){
     document.getElementById("frame").src += document.getElementById("id").value;
     document.getElementById("frame").style.display = "block";
     document.getElementById("form").style.display = "none";
+    document.body.style.margin = "0";
 }
