5f5b2d0f05118dfffbd2534bd908e5fc5823e09a
Yikuan Sun <yikuansun2015@gmail.com>
2021-01-05 17:29:56 -0500

    disabled scrolling in zoom

--- window/fs/Programs/ZOOM/app.js
+++ window/fs/Programs/ZOOM/app.js
@@ -1,5 +1,6 @@
 function joinMeeting(){
-    document.getElementById("frame").src += document.getElementById("id").value;
+    document.getElementById("frame").src += document.getElementById("id").value + "#unlogin-join-form";
+    document.getElementById("frame").scrolling = "no";
     document.getElementById("frame").style.display = "block";
     document.getElementById("form").style.display = "none";
     document.body.style.margin = "0";
