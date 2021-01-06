4ee7d4fe1cea193d3e21f150d163cc843605db0d
Yikuan Sun <yikuansun2015@gmail.com>
2021-01-05 22:21:50 -0500

    made a terminal that can excecute commands
    pretty simple and not good yet

---
+++ window/fs/Programs/Terminal/app.js
@@ -0,0 +1,30 @@
+document.body.style.margin = 0;
+document.getElementById("terminalpre").innerText += "Starting TARP terminal...";
+function typeBox() {
+    document.getElementById("terminalpre").innerText += "\nTARP: ";
+    input = document.createElement("code");
+    input.contentEditable = "true";
+    input.style.border = "none";
+    input.style.backgroundColor = "black";
+    input.style.color = "white";
+    document.getElementById("terminalpre").appendChild(input);
+    input.focus();
+    // prevent losing cursor
+    input.onblur = function(event) {
+        this.focus();
+    }
+    input.onkeydown = function(event) {
+        if (event.which == 13) {
+            window.parent.postMessage({
+                Name: this.innerText.split(" ")[0],
+                args: this.innerText.split(" ").splice(1, this.innerText.split("").length - 2)
+            });
+            setTimeout(typeBox, 500);
+            return false; // prevent adding a line break
+        }
+    }
+}
+setTimeout(typeBox, 2000);
+window.addEventListener("message", function(event) {
+    document.getElementById("terminalpre").innerText += "\n" + JSON.stringify(event.data);
+});
