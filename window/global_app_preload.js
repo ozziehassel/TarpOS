910be58e40c0e0be301717380f9677cd08d7a111
Yikuan Sun <yikuansun2015@gmail.com>
2021-04-21 20:37:15 -0400

    Update global_app_preload.js

--- window/global_app_preload.js
+++ window/global_app_preload.js
@@ -13,6 +13,6 @@ var raw_tarp_cmd = async function(command) {
 
 var tarpcommands = {};
 var avail_commands = ["savefile", "fetchsystemdata", "dir", "run", "readfile", "requestrestart", "setsettings", "github"];
-for (cmd of avail_commands) {
+for (var cmd of avail_commands) {
     tarpcommands[cmd] = new Function(`return raw_tarp_cmd({name:"${cmd}",args:JSON.parse(JSON.stringify(arguments))});`);
 }
